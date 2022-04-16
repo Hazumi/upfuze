3da605ebc08a399fa97ac65744ddff06a283acb2
zacharytyhacz <zactyh@gmail.com>
2022-04-16 17:34:51 -0400

    Added initial next + typescript setup

---
+++ upfuze/pages/signup.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+
+const SignUp: NextPage = () => {
+  return (
+    <div>
+        <h1>sign up</h1>
+        <h2>this should show sign in with github</h2>
+        <button type="button">
+            Sign In With Github
+        </button>
+    </div>
+  )
+}
+
+export default SignUp
