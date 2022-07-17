bd087b0c191a88deb1c2064d7e39285ac9f60e4e
zacharytyhacz <zactyh@gmail.com>
2022-07-17 13:30:22 -0400

    Added initial Sign In With Github button

--- upfuze/pages/signup.tsx
+++ upfuze/pages/signup.tsx
@@ -1,13 +1,33 @@
+import { useSession, signIn } from "next-auth/react"
 import type { NextPage } from 'next'
 
 const SignUp: NextPage = () => {
+  const session = useSession()
+
+  console.log(session)
+
+  const signInWithGithub = (): void => {
+    signIn("github", {
+      redirect: true,
+      redirect_uri: 'http://localhost:3000/signup'
+    })
+  }
+
+  if (session.data) {
+    return (
+      <div>
+        bro you logged in
+      </div>
+    )
+  }
+
   return (
     <div>
-        <h1>sign up</h1>
-        <h2>this should show sign in with github</h2>
-        <button type="button">
-            Sign In With Github
-        </button>
+      <h1>sign up</h1>
+      <h2>this should show sign in with github</h2>
+      <button type="button" onClick={signInWithGithub} title="Sign In With Github">
+          Sign In With Github
+      </button>
     </div>
   )
 }
