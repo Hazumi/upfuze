e7735c6c2bbbfdd2ca5a0eef5398ffee94953b5b
zacharytyhacz <zactyh@gmail.com>
2022-08-20 23:29:16 -0400

    Added showing latest 3 projects on index

--- upfuze/pages/index.tsx
+++ upfuze/pages/index.tsx
@@ -1,14 +1,40 @@
-import type { NextPage } from 'next'
+import type { NextPage, GetStaticPropsResult } from 'next'
+import { ProjectType } from 'upfuze'
+import Project from '../models/project'
+import { OneProjectCard } from '../components/OneProjectCard'
 
 import styles from '../styles/Home.module.css'
 
-const Home: NextPage = () => {
+type Props = {
+  latestProjects: ProjectType[]
+}
+
+const Home: NextPage<Props> = (props: Props) => {
+  const { latestProjects } = props
+
   return (
     <div className={styles.container}>
         <h1>upfuze</h1>
         <h2>coming soon</h2>
+        <hr />
+        <h2>Newest Projects</h2>
+        {
+          latestProjects.map(
+            (project) => <OneProjectCard key={project._id} project={project} />
+          )
+        }
     </div>
   )
+}
+
+export const getStaticProps = async (): Promise<GetStaticPropsResult<Props>> => {
+  const latestProjects = await Project.find({}).sort({ createdAt: -1 }).limit(3).lean()
+
+  return {
+    props: {
+      latestProjects: JSON.parse(JSON.stringify(latestProjects))
+    }
+  }
 }
 
 export default Home
